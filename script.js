58626193faba2dc36feea8600e5600b79fdb1b9b
Demid Kashirin <demid.kashirin@gmail.com>
2018-07-03 12:17:48 +0300

    add a script changing color for header (commented in DOM)

---
+++ script.js
@@ -0,0 +1,5 @@
+const header = document.getElementById('header-main');
+function changeHeaderColor(newColor) {
+  var header = document.getElementById('header-main');
+  header.style.color = newColor;
+}
