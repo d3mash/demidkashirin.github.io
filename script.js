26377e6273921643a9cb642542c6a8c602f7f893
Demid Kashirin <demid.kashirin@gmail.com>
2018-09-19 00:33:24 +0300

    remove unnecessary script

--- script.js
+++
@@ -1,5 +0,0 @@
-const header = document.getElementById('header-main');
-function changeHeaderColor(newColor) {
-  var header = document.getElementById('header-main');
-  header.style.color = newColor;
-}
